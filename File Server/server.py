76f2dc067ba614c264ee6f97f0ff529e83cec30f
jeffrynasri <jeffryfaruki@gmail.com>
2016-10-17 19:38:23 +0700

    FTP SErver dan filenya

---
+++ File Server/server.py
@@ -0,0 +1,25 @@
+import socket                   
+
+s = socket.socket()             # Inisialisasi Soket
+s.bind(('localhost', 10000))           #DItempelkan di locahost port 10000
+s.listen(1)                     
+
+print 'Server listening....'
+
+while True:
+    conn, addr = s.accept()     # Membangun KOneksi dengan klien
+    print 'Ada Koneksi dari', addr
+    data = conn.recv(1024)
+    print('Server received', repr(data))
+
+    filename='mytext.txt'
+    f = open(filename,'rb')
+    l = f.read(1024)
+    while (l):
+       conn.send(l)
+       print('MEngirim ',repr(l))
+       l = f.read(1024)
+    f.close()
+
+    print('Proses Download Sukses')
+    conn.close()
