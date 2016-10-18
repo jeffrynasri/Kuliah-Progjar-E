f7d3f5433be72c2a89050d3190cdb12cb4612993
MHR_7 <george_the@ymail.com>
2016-10-18 13:07:38 +0700

    tambah yang compatible buat py versi 3.5, ganti teks buat donwload

---
+++ File Server/server_ver35.py
@@ -0,0 +1,28 @@
+import socket                   
+
+def download_file(conn):
+    filename='mytext.txt'
+    f = open(filename,'rb')
+    l = f.read(1024)
+    while (l):
+       conn.send(l)
+       print('Mengirim ',repr(l))
+       l = f.read(1024)
+    f.close()
+
+    print('Proses Download Sukses')
+    conn.close()
+
+s = socket.socket()             # Inisialisasi Soket
+s.bind(('localhost', 10000))           #DItempelkan di locahost port 10000
+s.listen(1)                     
+
+print ('Server listening....')
+
+while True:
+    conn, addr = s.accept()     # Membangun KOneksi dengan klien
+    print ('Ada Koneksi dari', addr)
+    data = conn.recv(1024).decode('utf-8')
+    print('Server received', repr(data))
+
+    download_file(conn)
