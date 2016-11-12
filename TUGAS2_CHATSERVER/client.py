0277731075f05556004cd9e35b2724432e564582
Kukuh Rilo Pambudi <rilokukuh@gmail.com>
2016-11-13 03:25:37 +0700

    private message dengan username

---
+++ TUGAS2_CHATSERVER/client.py
@@ -0,0 +1,24 @@
+import socket
+from thread import *
+from time import sleep
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 8889))
+u = raw_input('Username: ')
+
+print 'Format pengiriman: recipient>message'
+def se(s):
+    while 1:
+        s.send(u + '>' + raw_input())
+        print s.recv(1024)
+def re(s):
+    while 1:
+        s.send(u + '>show>')
+        r = s.recv(1024)
+        if r != 'No messages':
+            print r
+        sleep(0.05)
+start_new_thread(se ,(s,))
+start_new_thread(re ,(s,))
+while 1:
+    pass
