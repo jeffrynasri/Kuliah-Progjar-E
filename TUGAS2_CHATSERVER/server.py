effc33ff4b3222d213d2c837ba72eacfb30b1379
jeffrynasri <jeffryfaruki@gmail.com>
2016-11-15 14:32:38 +0700

    Sudah bisa buat,tampil,hapus,gabung,keluar grup dan kirim pesan ke grup,private dengan menggunakan select

--- TUGAS2_CHATSERVER/server.py
+++ TUGAS2_CHATSERVER/server.py
@@ -1,7 +1,340 @@
 import socket
 from thread import *
 import string
+import select,sys
 
+#@PErintah
+#----------------------------------------------FUNGSI - FUNGSI DARI PERINTAH CHAT SERVER----------------------------------------------------------------
+def login(socket,username,password):#OK
+    try:
+        for i in range(0,len(ACCOUNT_LIST),1):
+            if(i==(len(ACCOUNT_LIST)-1) and ACCOUNT_LIST[i][0]!=username and ACCOUNT_LIST[i][1]!=password):
+                kirim_status(sock,2)            
+            if(username==ACCOUNT_LIST[i][0] and password==ACCOUNT_LIST[i][1]):
+                LOGIN_SESSION[CONNECTION_LIST.index(socket)]=username
+                GROUP_SESSION[CONNECTION_LIST.index(socket)]="Publik"
+                broadcast_data(socket, "[" +str(socket.getpeername())+ ":"+username+"] memasuki grup publik \r\n" )
+                kirim_status(socket,1)
+                kirim_status(socket,12)
+                break
+                
+    except:
+        kirim_status(socket,2)            
+def daftar(socket,username,password):#Ok
+    try:
+        for i in range(0,len(ACCOUNT_LIST),1):
+            if(username==ACCOUNT_LIST[i][0] and password==ACCOUNT_LIST[i][1]):
+                return
+        f=open("akun_chat.txt","a+")
+        f.write(username + " " + password +"\r\n")
+        ACCOUNT_LIST.append([])
+        ACCOUNT_LIST[(len(ACCOUNT_LIST))-1].append(username)
+        ACCOUNT_LIST[(len(ACCOUNT_LIST))-1].append(password)
+        f.close()
+        kirim_status(socket,8)      
+    except:
+        kirim_status(socket,9)          
+def keluar(socket):
+    socket.close()
+    CONNECTION_LIST.remove(socket)
+def logout(socket):#OK
+    try:
+        LOGIN_SESSION[CONNECTION_LIST.index(socket)]="0"
+        GROUP_SESSION[CONNECTION_LIST.index(socket)]="0"
+        kirim_status(socket,6)              
+    except:
+        kirim_status(socket,7)              
+def kirim_grup(sock,isipesan):#ok
+    nama_grup=GROUP_SESSION[CONNECTION_LIST.index(sock)]
+    #print(nama_grup)
+    try :    
+        for socket in CONNECTION_LIST:
+            if (GROUP_SESSION[CONNECTION_LIST.index(socket)] == nama_grup and socket != sock) :
+                    socket.send("\r\n"+isipesan)              
+        kirim_status(sock,3)                            
+    except :
+        kirim_status(socket,4)              
+        socket.close()
+        CONNECTION_LIST.remove(socket)
+
+def kirim_private(sock,username_tujuan,isipesan):#ok
+    try:
+        for i in range(0,len(LOGIN_SESSION),1):
+            if(i==(len(LOGIN_SESSION)-1) and LOGIN_SESSION[i][0]!=username_tujuan):
+            #JIka User tdk ditemukan
+                kirim_status(sock,4)            
+            elif(LOGIN_SESSION[i]==username_tujuan):
+                CONNECTION_LIST[i].send("\r\n"+isipesan)
+                kirim_status(sock,3)
+                break
+    except:                
+        kirim_status(sock,4)
+def bgrup(sock,nama,password):
+    try:
+        for i in range(0,len(GROUP_LIST),1):
+            if(nama==GROUP_LIST[i][0] and password==GROUP_LIST[i][1]):
+                return
+        fg=open("grup_chat.txt","a+")
+        fg.write(nama + " " + password +"\r\n")
+        GROUP_LIST.append([])
+        GROUP_LIST[(len(GROUP_LIST))-1].append(nama)
+        GROUP_LIST[(len(GROUP_LIST))-1].append(password)
+        fg.close()
+        kirim_status(sock,13)      
+    except:
+        kirim_status(sock,14)
+
+def pgs(sock):#ok
+    try:
+        sock.send("\r\n" + GROUP_SESSION[CONNECTION_LIST.index(sock)]+"\r\n")
+        kirim_status(sock,20)
+    except:
+        kirim_status(sock,21)    
+def ggrup(sock,nama,password):#ok
+    fg=open("grup_chat.txt","r")    
+    try:    
+        for line in fg:
+            if(line==(nama+" "+password+'\n')):
+                GROUP_SESSION[CONNECTION_LIST.index(sock)]=nama
+                kirim_status(sock,12)
+            
+        fg.close()
+    except:
+        kirim_status(sock,15)
+def tgrup(sock): #ok
+    data=""
+    try:
+        for i in range(0,len(GROUP_LIST),1):
+            data=data+str(i+1)+". "+ GROUP_LIST[i][0] +" \r\n"
+        sock.send("\r\n"+data)
+        kirim_status(sock,20)
+    except:
+        pkirim_status(sock,21)
+def kgrup(sock):#ok
+    try:
+        GROUP_SESSION[CONNECTION_LIST.index(sock)]="Publik"
+        kirim_status(sock,16)
+    except:
+        kirim_status(sock,17)
+def hgrup(sock,nama,password):  
+    try:
+        fg=open("grup_chat.txt","r")    
+        data=""
+        for line in fg:
+            if(line==(nama+" "+password+'\n')):
+                continue
+            data=data+line
+       
+        fg.close()   
+        if data: 
+        #HApus data grup di grup_chat.txt
+            fg=open("grup_chat.txt","w")    
+            fg.write(data)
+            fg.close()
+        #---------------------------------
+        #Ubah default data grup di GROUP_SESSION
+            for i in range(i,len(GROUP_SESSION),1):
+                if GROUP_SESSION[i]==nama :
+                    GROUP_SESSION[i]="Publik"
+        #-------------------------------    
+        #HApus data grup di GROUP_LIST
+            for i in range(i,len(GROUP_LIST),1):
+                if GROUP_LIST[i][0]==nama :
+                    del GROUP_LIST[i]    
+        kirim_status(sock,18)
+    #-------------------------------    
+    except :
+        kirim_status(sock,19)
+
+#@PEmrosesan
+#----------------------------------------------FUNGSI - FUNGSI PEMROSESAN----------------------------------------------------------------
+def broadcast_data (sock, message):
+    for socket in CONNECTION_LIST:
+        if socket != server_socket and socket != sock :
+            try :
+                socket.send(message)              
+            except :  
+                socket.close()
+                CONNECTION_LIST.remove(socket)
+                        
+def kirim_status(conn,kode):
+    if(kode == 1):
+        deskripsi = green+ str(kode)+ ":" +'Login Berhasil.'+ color_tail
+    if(kode == 2):
+        deskripsi = red+ str(kode)+ ":" + 'Login Gagal.'+ color_tail
+    if(kode == 3):
+        deskripsi = green+ str(kode)+ ":"  + 'Kirim Pesan Berhasil.' +color_tail 
+    if(kode == 4):
+        deskripsi = red+ str(kode)+ ":"  + 'Kirim Pesan Gagal.' +color_tail
+    if(kode == 5):
+        deskripsi = red+ str(kode)+ ":"  + 'Proses Gagal,Karena Belum Terautentifikasi.' +color_tail                            
+    if(kode == 6):
+        deskripsi = green+ str(kode)+ ":"  + 'Logout Berhasil.' +color_tail 
+    if(kode == 7):
+        deskripsi = red+ str(kode)+ ":"  + 'Logout Gagal.' +color_tail
+    if(kode == 8):
+        deskripsi = green+ str(kode)+ ":" + 'Daftar Berhasil.' +color_tail 
+    if(kode == 9):
+        deskripsi = red+ str(kode)+ ":"  + 'Daftar Gagal.' +color_tail
+    if(kode == 10):
+        deskripsi = red+ str(kode)+ ":"  + 'Syntax Tidak Dikenali.' +color_tail
+    if(kode == 11):
+        deskripsi = green+ str(kode)+ ":"  + 'Join Chat Server Sukses.' +color_tail                                
+    if(kode == 12):
+        deskripsi = blue + "\r\n<----------Selamat Datang di Grup "+GROUP_SESSION[CONNECTION_LIST.index(conn)]+ " ---------->" +color_tail                                
+    if(kode == 13):
+        deskripsi = green+ str(kode)+ ":"  + 'Buat Grup Sukses.' +color_tail
+    if(kode == 14):
+        deskripsi = red+ str(kode)+ ":"  + 'Buat Grup Gagal.' +color_tail
+    if(kode == 15):
+        deskripsi = red + str(kode)+ ":" + 'Gabung Grup Gagal.' +color_tail
+    if(kode == 16):
+        deskripsi = green + str(kode)+ ":" + 'Keluar Grup Berhasil.' +color_tail
+    if(kode == 17):
+        deskripsi = red + str(kode)+ ":" + 'Keluar Grup Gagal.' +color_tail
+    if(kode == 18):
+        deskripsi = green + str(kode)+ ":" + 'Hapus Grup Berhasil.' +color_tail
+    if(kode == 19):
+        deskripsi = red + str(kode)+ ":" + 'Hapus Grup Gagal.' +color_tail
+    if(kode == 20):
+        deskripsi = green + str(kode)+ ":" + 'Data Berhasil Diterima.' +color_tail
+    if(kode == 21):
+        deskripsi = green + str(kode)+ ":" + 'Data Gagal Diterima.' +color_tail                            
+    
+    menu="[ChatsrvrE1-"
+    if(LOGIN_SESSION[CONNECTION_LIST.index(conn)]=="0"):
+        menu=menu + "User = Belum Login] > " 
+    else:
+        menu=menu+"User = " + LOGIN_SESSION[CONNECTION_LIST.index(conn)] + "] > "                                                         
+
+    if (kode==1):
+        conn.send(deskripsi+"\r\n")
+    else:
+        conn.send(deskripsi+"\r\n"+menu)
+def menu_handling(sock):
+    if sock == server_socket:
+        koneksi_client,alamat_client= server_socket.accept()
+        CONNECTION_LIST.append(koneksi_client)
+        LOGIN_SESSION.append("0")
+        GROUP_SESSION.append("0")        
+        print "Koneksi dari ",alamat_client
+        koneksi_client.send("-------Chat Server Kelompok 1-Progjar E-------\r\n       ---------------------       \r\n1. JORDY ADHITYO P 5111100101\r\n2. Jeffry Nasri Faruki 5114100043\r\n3. R.AY. Noormal Nadya 5114100127\r\n4. M Habibur Rahman 5114100163\r\n5. Kukuh Rilo Pambudi 5114100178\r\n-----------------------------------\r\nKetik login <username> <password> untuk login\r\n-----------------------------------\r\n")
+        kirim_status(koneksi_client,11)                         
+    else:
+        # Data recieved from client, process it
+        try:
+#LOGIN_SESSION[CONNECTION_LIST.index(sock)]!="0"'''        
+            message = sock.recv(1024)
+            if message:
+                perintah=message.split()      
+                if(LOGIN_SESSION[CONNECTION_LIST.index(sock)]!="0"):            
+                    #PErintah2 Yang PErlu AUtentifikasi                    
+                    if (perintah[0].lower() == 'logout'):
+                        logout(sock)                     
+                    elif (perintah[0].lower() == 'kirim_private'):
+                        kirim_private(sock,perintah[1],'<' + LOGIN_SESSION[CONNECTION_LIST.index(sock)] + '> ' + perintah[2] + "\r\n")                
+                    elif (perintah[0].lower() == 'keluar'):
+                        keluar(sock)                        
+                    elif (perintah[0].lower() == 'bgrup'):
+                        bgrup(sock,perintah[1],perintah[2])                   
+                    elif (perintah[0].lower() == 'ggrup'):
+                        ggrup(sock,perintah[1],perintah[2])
+                    elif (perintah[0].lower() == 'tgrup'):
+                        tgrup(sock)                  
+                    elif (perintah[0].lower() == 'kgrup'):
+                        kgrup(sock)      
+                    elif (perintah[0].lower() == 'pgs'):
+                        pgs(sock)                                                      
+                    elif (perintah[0].lower() == 'hgrup'):
+                        hgrup(sock,perintah[1],perintah[2])                    
+                    elif (perintah[0].lower() == 'kirim_grup'):
+                        kirim_grup(sock,'<' + LOGIN_SESSION[CONNECTION_LIST.index(sock)] + '> ' + perintah[1] + "\r\n")                
+                    else:
+                        kirim_status(sock,10)
+                else:
+                    #PErintah2 TIdak PErlu Autentifikasi
+                    if (perintah[0].lower() == 'login'):
+                        login(sock,perintah[1],perintah[2])
+                    elif (perintah[0].lower() == 'daftar'):
+                        daftar(sock,perintah[1],perintah[2])
+                    elif (perintah[0].lower() == 'logout' or perintah[0].lower() == 'kirim_private' or perintah[0].lower() == 'bgrup'or perintah[0].lower() == 'ggrup'or perintah[0].lower() == 'tgrup'or perintah[0].lower() == 'kgrup'or perintah[0].lower() == 'hgrup'or perintah[0].lower() == 'kirim_grup'):
+                        kirim_status(sock,5)
+                    elif (perintah[0].lower() == 'keluar'):                        
+                        keluar(sock)
+                    else:
+                        kirim_status(sock,10)
+        except:
+                broadcast_data(sock, "Client (%s, %s) sedang offline" % alamat_client)
+                print "Client (%s, %s) sedang offline" % alamat_client
+                sock.close()
+                CONNECTION_LIST.remove(sock)
+
+def inisialitation():
+    # INsialisasi soket server
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(srver_address)
+    server_socket.listen(10)
+    CONNECTION_LIST.append(server_socket)
+    LOGIN_SESSION.append("0")
+    GROUP_SESSION.append("0")
+    #--------------------------
+    # Inisialisasi DAftar Akun
+    f=open("akun_chat.txt","r")
+    i=0    
+    for line in f:
+        akun=line.split()
+        if akun:
+            ACCOUNT_LIST.append([])
+            ACCOUNT_LIST[i].append(akun[0])           
+            ACCOUNT_LIST[i].append(akun[1])                   
+            i=i+1
+    f.close()
+    #---------------------------                  
+    # Inisialisasi DAftar Grup
+    fg=open("grup_chat.txt","r")
+    i=0    
+    for line in fg:
+        grup=line.split()
+        if grup:
+            GROUP_LIST.append([])
+            GROUP_LIST[i].append(grup[0])           
+            GROUP_LIST[i].append(grup[1])                   
+            i=i+1
+    fg.close()
+    #---------------------------      
+
+#@Main
+#----------------------------------------------FUNGSI MAIN----------------------------------------------------------------                
+if __name__ == "__main__":
+     
+    CONNECTION_LIST = []    # DAftar PEngguna meliputi klien + server
+    ACCOUNT_LIST=[]         #DAftar AKun dg Array 2d (kolom 1 username dan  kolom 2 password)
+    GROUP_LIST=[]         #DAftar AKun dg Array 2d (kolom 1 nama grup dan  kolom 2 PAssword)
+    LOGIN_SESSION=[]        #Loggin SEssion dg Array 1d(Isi nya adalah 0 untuk belum login dan selain 0(username) untuk yg sudah login)   
+    GROUP_SESSION=[]         #Grup SEssion dg Array 1d(Isi nya adalah 0 untuk yg belumjoin dan selain 0(namagrup) untuk yg sudah join grup)   
+    srver_address=('localhost',10000) 
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    green='\033[1;32m'
+    blue = '\033[1;34m'
+    red='\033[1;91m'
+    yellow='\033[1;33m'
+    underline = '\033[1;4m'    
+    color_tail='\033[1;m'
+
+    inisialitation()  
+#    print (ACCOUNT_LIST)
+    print "Dijalankan di interface %s port %s "%srver_address
+    while True:
+        print >> sys.stderr, 'Menunggu Koneksi'
+        read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[])
+ 
+        for sock in read_sockets:
+            menu_handling(sock)
+
+     
+    server_socket.close()
+
+'''
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 address=('127.0.0.1', 9011)
 s.bind(address)
@@ -73,3 +406,4 @@ while 1:
     print 'ada koneksi'
 
 s.close()
+'''
